18d84b4620a5b733ed3eeb2f835a6e8d4a7a7478
Imad Rajwani <imadrajwani@gmail.com>
2019-10-03 18:39:19 +0500

    first

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'jade'); // configure template engine
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+//Configure Mongoose
+// mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.connect('mongodb+srv://nottmystyle-cluster-cxrid.mongodb.net/admin');
+mongoose.set('debug', true);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
